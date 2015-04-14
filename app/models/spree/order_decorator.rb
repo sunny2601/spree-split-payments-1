c7ffae87f0cbec9411e8cef6bcee70f8112571c7
EJ Etherington <ej.etherington@instrument.com>
2015-04-14 15:10:01 -0700

    updated to work better with all the ins and outs of the checkou process accounting for other payment types

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -53,7 +53,19 @@ Spree::Order.class_eval do
 
   def update_params_payment_source
     if has_checkout_step?("payment") && self.payment?
+      # if existing card, make sure it is the first payemnt in the index
+      @existing_card_id = @updating_params[:order] ? @updating_params[:order][:existing_card] : nil
+
       insert_source_params
+      # If existing card, rotate the array so the partial
+      # payment won't be the fist position in the
+      # array. This is important because of the next step
+      if @existing_card_id.present?
+        @updating_params[:order][:payments_attributes].rotate!
+      end
+
+      return unless @updating_params[:order].key?(:payments_attributes)
+
       #set order total after partial payments on the first remaining non-partial payment method
       @updating_params[:order][:payments_attributes].each_with_index do |payments_attrs, index|
         unless Spree::PaymentMethod.find(payments_attrs[:payment_method_id]).for_partial?
@@ -71,23 +83,30 @@ Spree::Order.class_eval do
     # force procesing of partial paymets first
     insert_source_params_for_partial_payments
 
-    if order_total_after_partial_payments > 0
+    if order_total_after_partial_payments > 0.00
       insert_souce_params_for_other_payments
+    end
+
+    # if there are no payments in the submission
+    # then remove the payments attributes
+    if @updating_params[:order][:payments_attributes] == []
+      @updating_params[:order].delete(:payments_attributes)
     end
 
     @updating_params.delete(:payment_source)
   end
 
   def insert_souce_params_for_other_payments
-    # not do non-partials if it still necessary
     @updating_params[:payment_source].each do |payment_method_id,payment_source_attributes|
       unless Spree::PaymentMethod.find(payment_method_id).for_partial?
-        payments_attributes = {
-          payment_method_id: payment_method_id,
-          not_to_be_invalidated: true,
-          source_attributes: payment_source_attributes
-        }
-        @updating_params[:order][:payments_attributes] <<  payments_attributes
+        if (payment_source_attributes[:number].empty? && !@existing_card_id.nil?) || !payment_source_attributes[:number].empty?
+          payments_attributes = {
+            payment_method_id: payment_method_id,
+            not_to_be_invalidated: true,
+            source_attributes: payment_source_attributes
+          }
+          @updating_params[:order][:payments_attributes] <<  payments_attributes
+        end
       end
     end
   end
@@ -102,13 +121,16 @@ Spree::Order.class_eval do
         }
         #dont set payments_attributes to the order if the gift card number is empty
         #currently assuming that the only partial payment will be a valutec card
-        if !payment_source_attributes[:number].empty? 
+        unless payment_source_attributes[:number].empty?
           card = Stumptown::Valutec::Card.new(
             card_number: payment_source_attributes[:number]
             )
           available_funds = card.balance.result
           if available_funds >= outstanding_balance
             payments_attributes[:amount] = outstanding_balance
+
+            @updating_params[:order].delete(:existing_card) if @existing_card_id
+          
           else
             payments_attributes[:amount] = available_funds
           end
@@ -123,6 +145,6 @@ Spree::Order.class_eval do
     @updating_params[:order][:payments_attributes].each do |payments_attributes|
       amount += payments_attributes[:amount].to_f
     end
-    outstanding_balance - amount
+    (outstanding_balance - amount).round(2)
   end
 end
